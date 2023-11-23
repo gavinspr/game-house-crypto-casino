3d32ec98353a6b92810bd805987f85bf80c04115
gspro <gavin.sproles@gmail.com>
2023-11-23 15:07:29 -0500

    init hardhat project

---
+++ scripts/deploy.ts
@@ -0,0 +1,10 @@
+import { formatEther, parseEther } from "viem";
+import hre from "hardhat";
+
+async function main() {
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
